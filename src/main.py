6378661afd35ccb2bf837445e6b850d7a8d43180
Willem Meijer <w.meijer.5@student.rug.nl>
2022-12-08 17:12:14 +0000

    using env variables for topic and endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+"""
+Implements very simple external task handler.
+"""
+
+import os
+
 
 from camunda.external_task.external_task_worker import ExternalTaskWorker
 from camunda.external_task.external_task import ExternalTask, TaskResult
 from camunda.client.engine_client import EngineClient
+
+ENDPOINT_KEY = "ENDPOINT"
+TOPIC_KEY = "TOPIC"
 
 
 def task_handler(task: ExternalTask) -> TaskResult:
@@ -9,11 +18,12 @@ def task_handler(task: ExternalTask) -> TaskResult:
 
 
 if __name__ == "__main__":
-    # TODO: replace with environment variable.
-    BASE_URL = "http://cam-workflow:8080/engine-rest"
-    TOPIC_NAME = "topicName"
+    ENDPOINT = os.getenv(ENDPOINT_KEY)
+    BASE_URL = f'http://{ENDPOINT}/engine-rest'
+    TOPIC_NAME = os.getenv(TOPIC_KEY)
+
+    print(f'{BASE_URL=}\n{TOPIC_NAME=}')
 
     client = EngineClient(BASE_URL)
     worker = ExternalTaskWorker(worker_id=0, base_url=BASE_URL)
     worker.subscribe(TOPIC_NAME, task_handler)
-    
