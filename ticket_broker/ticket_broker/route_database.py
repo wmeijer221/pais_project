63fef86f201f6fe2c7a51cae33727488e20343f1
David Visscher <david@davidvisscher.nl>
2022-12-13 01:08:15 +0100

    Working on the ORM. relationships not working yet

--- ticket_broker/ticket_broker/route_database.py
+++ ticket_broker/ticket_broker/route_database.py
@@ -1,5 +1,5 @@
 import sqlalchemy as db
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, Mapped
 
 _ENGINE = db.create_engine("sqlite:///data/railways.sqlite3")
 _METADATA = db.MetaData()
@@ -12,28 +12,30 @@ class Station(Base):
     __tablename__ = "stations"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column( db.Text)
-    slug = db.Column( db.Text)
+    name = db.Column(db.Text)
+    slug = db.Column(db.Text)
     latitude = db.Column(db.REAL)
     longitude = db.Column(db.REAL)
     country = db.Column(db.Text)
     time_zone = db.Column(db.Text)
     is_city = db.Column(db.Text)
-    is_main_station = db.Column( db.Text)
+    is_main_station = db.Column(db.Text)
     is_airport = db.Column(db.Text)
     country_hint = db.Column(db.Text)
     same_as = db.Column(db.Integer)
     normalised_code = db.Column('normalised_code', db.Text)
     iata_airport_code = db.Column('iata_airport_code', db.Text)
 
-    routes = relationship("Route")
+    routes = relationship("Route", primaryjoin="or_(Route.start_station==Station.id,Route.end_station==Station.id )")
+
 
 class Route(Base):
     __tablename__ = "routes"
 
     id = db.Column(db.Integer, primary_key=True)
-    start_station = db.Column(db.Integer, db.ForeignKey('station.id'))
-    end_station = db.Column(db.Integer, db.ForeignKey('station.id'))
+    start_station = db.Column(db.Integer, db.ForeignKey('stations.id'))
+    end_station = db.Column(db.Integer, db.ForeignKey('stations.id'))
+
     traveltime_seconds = db.Column(db.Integer)
     price_eurocents_firstclass = db.Column(db.Integer)
     price_eurocents_economy = db.Column(db.Integer)
