50c957d609b204a0bd68d18295297ff9b4e34504
David Visscher <david@davidvisscher.nl>
2022-12-18 20:05:29 +0100

    add logic for loading into an nx graph and some tests

--- ticket_broker/test/test_route_database.py
+++ ticket_broker/test/test_route_database.py
@@ -1,6 +1,27 @@
 from ticket_broker.route_database import RouteDatabase
 
+TEST_STATIONS = {
+    "A": {
+        "pretty_name": "Station A",
+        "country": "AA"
+    },
+    "B": {
+        "pretty_name": "Station B",
+        "country": "BB"
+    },
+    "C": {
+        "pretty_name": "Station C",
+        "country": "CC"
+    }
+}
 
 def test_route_database_creatable():
     assert RouteDatabase([], {}, {}) is not None
     assert RouteDatabase.from_file("data/railways.yaml") is not None
+
+def test_load_notes():
+    rdb = RouteDatabase([], TEST_STATIONS, {})
+    assert "A" in rdb.graph
+    assert "B" in rdb.graph
+    assert "C" in rdb.graph
+    assert "D" not in rdb.graph
--- ticket_broker/ticket_broker/route_database.py
+++ ticket_broker/ticket_broker/route_database.py
@@ -1,6 +1,8 @@
+import copy
 from typing import List, Dict
 from pathlib import Path
 
+import networkx as nx
 import yaml
 
 
@@ -9,13 +11,61 @@ class RouteDatabase:
     Object that holds and constructs a network graph of all known routes.
     Can also be asked to generate routes based on certain criteria.
     """
+
     def __init__(self, routes: List[Dict], stations: Dict, companies: Dict):
         """
         Creates a route database, loading it into a graph from a list.
         """
-        self.routes = routes
-        self.stations = stations
-        self.companies = companies
+        self._routes = routes
+        self._stations = stations
+        self._companies = companies
+        self.graph = self._init_network_graph()
+
+    def _init_network_graph(self, routes = None, stations = None):
+        """
+        Method for loading in the routing information into a networkx graph.
+        That graph can then be used to determine the different pathing options.
+        """
+        if stations is None:
+            stations = self._stations
+        if routes is None:
+            routes = self._routes
+
+        graph = nx.MultiGraph()
+        self._load_nodes(stations, graph=graph)
+        self._load_routes(routes, graph=graph)
+
+        return graph
+
+    def _load_nodes(self, stations: Dict, graph = None):
+        """
+        Loads a set of nodes into the local graph.
+        """
+        if graph is None:
+            graph = self.graph
+
+        for key, values in stations.items():
+            graph.add_node(key, **values)
+
+        return graph
+
+    def _load_routes(self, routes: List[Dict], graph = None):
+        """
+        Loads a set of routes into the existing network graph.
+        It is expected that the nodes have already been loaded using _load_nodes.
+        Failure to do so will result in missing metadata.
+        """
+        if graph is None:
+            graph = self.graph
+
+        for route in routes:
+            route_metadata = copy.deepcopy(route)
+            route_metadata.pop('start_station')
+            route_metadata.pop('end_station')
+            graph.add_edge(route['start_station'],
+                           route['end_station'],
+                           **route_metadata)
+        return graph
 
     @classmethod
     def from_file(cls, file_path: Path):
