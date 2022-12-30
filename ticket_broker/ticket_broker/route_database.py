89c027b0a35ab223598507fd2280bd92f5edc27b
David Visscher <david@davidvisscher.nl>
2022-12-31 00:36:20 +0100

    Pathfinding now works!

--- ticket_broker/ticket_broker/route_database.py
+++ ticket_broker/ticket_broker/route_database.py
@@ -21,7 +21,48 @@ class RouteDatabase:
         self._companies = companies
         self.graph = self._init_network_graph()
 
-    def _init_network_graph(self, routes = None, stations = None):
+    def find_route_options(self, start_station, end_station, weight):
+        """
+        Finds a route from one station to another.
+
+        start_station: The station to start the journey.
+        end_station: The station to end the journey.
+        weight: the name of the edge weight to use. Look in railways.yaml for the metadata with each route.
+
+        For now, we only return one option.
+        """
+        intermediate_stations = nx.shortest_path(self.graph, start_station, end_station, weight=weight)
+
+        legs = []
+
+        for i in range(len(intermediate_stations) - 1):
+            leg_start = intermediate_stations[i]
+            leg_end = intermediate_stations[i+1]
+
+            leg_data = {"start_station": leg_start,
+                        "end_station": leg_end,
+                        "route_data": {}}
+
+            edges = self.graph.get_edge_data(leg_start, leg_end)
+
+            print(f"LEG DATA FROM {leg_start} to {leg_end}:\n EDGES: {edges}")
+
+            if len(edges) == 1:
+                best_option = 0
+            else:
+                # If we have multiple choices for one leg, we choose the right one by the weight
+                best_option = 0
+                best_weight_value = edges[0][weight]
+                for key, edge in edges.items():
+                    if edge[weight] < best_weight_value:
+                        best_option = key
+
+            leg_data["route_data"] = edges[best_option]
+            legs.append(leg_data)
+
+        return [legs]
+
+    def _init_network_graph(self, routes=None, stations=None):
         """
         Method for loading in the routing information into a networkx graph.
         That graph can then be used to determine the different pathing options.
@@ -37,7 +78,7 @@ class RouteDatabase:
 
         return graph
 
-    def _load_nodes(self, stations: Dict, graph = None):
+    def _load_nodes(self, stations: Dict, graph=None):
         """
         Loads a set of nodes into the local graph.
         """
@@ -49,7 +90,7 @@ class RouteDatabase:
 
         return graph
 
-    def _load_routes(self, routes: List[Dict], graph = None):
+    def _load_routes(self, routes: List[Dict], graph=None):
         """
         Loads a set of routes into the existing network graph.
         It is expected that the nodes have already been loaded using _load_nodes.
--- ticket_broker/ticket_broker/worker.py
+++ ticket_broker/ticket_broker/worker.py
@@ -23,9 +23,9 @@ client = ZeebeClient(channel)
 @worker.task(task_type="send_journey_specification")
 async def send_journey_specification(journey_specification: Dict, order_id: str):
     """
-    Finds routes based on input
+    Sends the message to initialise the broker pool.
     """
-    print(f"Received job: {journey_specification}")
+    print(f"Received job: send_journey_specification : {journey_specification}")
 
     message = {
         "order_id": order_id,
@@ -34,6 +34,28 @@ async def send_journey_specification(journey_specification: Dict, order_id: str)
 
     print("publishing message to ticket broker")
     await client.publish_message("find_journey", str(order_id), message)
+
+
+@worker.task(task_type="find_route_options")
+async def find_route_options(journey_specification: Dict, order_id: str):
+    """
+    Finds route options and sends it to the customer.
+    """
+    print(f"Received job: find_route_options : {journey_specification} {order_id}")
+
+    if journey_specification['class'] == "first":
+        weight = 'price_eurocents_firstclass'
+    else:
+        weight = 'price_eurocents_economy'
+
+    options = rdb.find_route_options(journey_specification['start_station'],
+                                     journey_specification['end_station'],
+                                     weight)
+
+    message = {"route_options": options}
+    await client.publish_message("receive_journey_options", str(order_id), message)
+
+    return message
 
 
 def run_loop():
